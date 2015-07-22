a768ad06d501a97f0f3bb682fd955e1b24e2b07b
Dan Russell <dan@adyapper.com>
2015-07-21 20:52:32 -0400

    Added comments.

--- cachematrix.R
+++ cachematrix.R
@@ -1,23 +1,26 @@
 ## The two functions below handle construction, inversion and caching of matrix objects.
 
 ## makeCacheMatrix creates a matrix object that can cache its inverse.
-
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
   
+  #Cache the matrix with a Null value for the inverted/solved matrix since it hasn't been computed yet.
   set <- function(y) {
     x <<- y
     inv <<- NULL
   }
   
+  #Returns the matrix
   get <- function() {
     x
   }
   
+  #Sets the inverted value in cache
   setinverse <- function(inverse) {
     inv <<- inverse
   } 
   
+  #Retrieves the inverted value from cache (returns NULL if it hasn't been previously set)
   getinverse <- function() {
     inv
   }
@@ -35,7 +38,7 @@ cacheSolve <- function(x, ...) {
     return(inverse)
   }
   
-  #No cached version, need to get the matrix, solve it, and cache the inverted.
+  #No cached inverted matrix: need to get the matrix, solve it (invert), cache the inverted, and return it.
   data <- x$get()
   
   inv <- solve(data, ...)
